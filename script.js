0ee360ed9dd5ce2cec3db8449844b88ea6e0aaf4
Ysweb3 <yssh88972@gmail.com>
2024-08-14 22:33:19 +0530

    Add form pop-up code

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const openBtn = document.getElementById("form-open");
+const closeBtn = document.getElementById("form-close");
+const form = document.getElementById("form-pop-container");
+
+openBtn.addEventListener("click", () =>{
+    form.classList.add("open");
+});
+closeBtn.addEventListener("click", () =>{
+    form.classList.remove("open");
+});
+
+
+
+
+
+
 const myLibrary = [
-    
+
 ];
 
 function Book() {
