e8a0d097ad9b5f66765651206055f4ecfa83149c
Ysweb3 <yssh88972@gmail.com>
2024-08-26 12:11:38 +0530

    Add better book display function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const closeBtn = document.getElementById("form-close");
 const formContainer = document.getElementById("form-pop-container");
 const container = document.getElementById("container");
 
+
 openBtn.addEventListener("click", () =>{
     formContainer.classList.add("open");
     container.style.filter = "blur(3px)";
@@ -13,138 +14,66 @@ closeBtn.addEventListener("click", () =>{
 });
 
 
+
 const booksContainer = document.getElementById("bookscontainer");
 const addBtn = document.getElementById("form-open");
 const form = document.getElementById("form-pop");
 const submitBtn = document.getElementById("add-book");
+const readBtn = document.getElementById("form-read-button");
+let bookRead = false; 
+readBtn.style.backgroundColor = "red"
 
-
-const myLibrary = [
-  {
-    title:"1984",
-    pages:328,
-    author:"George Orwell",
-    isRead:true
-  },
-  {
-    title:"The Greate Gatsby",
-    pages:180,
-    author:"F. Scott Fitzgerald",
-    isRead:true
-  },
-  {
-    title:"One Hundred Years of Solitude",
-    pages:417,
-    author:"Gabriel Garcia Marquez",
-    isRead:false
-  },
-  {
-    title:"Pride and Prejudice",
-    pages:279,
-    author:"Jane Austen",
-    isRead:true
-  },
-  
-
-];
-
-function bookList(title,pages,author,isRead) {
-  this.title = title;
-  this.pages = pages;
-  this.author = author;
-  this.isRead = isRead;
-}
-
-function displayBooks(){
-  for (let i = 0; i < myLibrary.length; i++) {
+function addBook(){
     const book = document.createElement("div");
     book.setAttribute("class","book");
-    
+    booksContainer.appendChild(book);  
+
+    let newTitle = document.getElementById("title").value;
+    let newAuthor = document.getElementById("author").value;
+    let newPages = document.getElementById("pages").value;
+
     const title = document.createElement("p");
     title.setAttribute("class", "title");
-    title.innerHTML = "<strong>Title:</strong>" + myLibrary[i].title;
+    title.innerHTML = "<strong>Title:</strong>" + newTitle;
+    book.appendChild(title);
               
     const author = document.createElement("p");
     author.setAttribute("class", "author");
-    author.innerHTML = "<strong>Author:</strong>" + myLibrary[i].author;
+    author.innerHTML = "<strong>Author:</strong>" + newAuthor;
+    book.appendChild(author);
     
     const pages = document.createElement("p");
     pages.setAttribute("class", "pages");
-    pages.innerHTML = "<strong>Pages:</strong>" + myLibrary[i].pages;
-
-    const status = document.createElement("p");
-    status.setAttribute("class", "read");
-    status.innerHTML = "<strong>Status:</strong>" + myLibrary[i].isRead
-
-    const bookButtons = document.createElement("p");
-    bookButtons.setAttribute("class","book-buttons")
-    bookButtons.innerHTML = '<button class="read-button"><img src="assets/svg/book-open-svgrepo-com.svg" alt="Read" height="25px"></button><button class="remove-button"><img src="assets/svg/trash-xmark-alt-svgrepo-com.svg" alt="Delete" height="25px"></button>';
-
-    booksContainer.appendChild(book);
-    book.appendChild(title);
-    book.appendChild(author);
+    pages.innerHTML = "<strong>Pages:</strong>" + newPages;
     book.appendChild(pages);
-    book.appendChild(status);
-    book.appendChild(bookButtons);
-    booksContainer.appendChild(addBtn);
-  }
-}
 
-function displayNewBook(Btitle,Bauthor,Bpages){
-  const book = document.createElement("div");
-    book.setAttribute("class","book");
+    const read = document.createElement("p");
+    read.setAttribute("class", "pages");
+    if (bookRead == true) {
+        read.innerHTML = "<strong>Status:</strong>Read";
+    } else {
+        read.innerHTML = "<strong>Status:</strong>Not Read";
+    }
+    book.appendChild(read);
     
-    const title = document.createElement("p");
-    title.setAttribute("class", "title");
-    title.innerHTML = "<strong>Title:</strong>" + Btitle;
-              
-    const author = document.createElement("p");
-    author.setAttribute("class", "author");
-    author.innerHTML = "<strong>Author:</strong>" + Bauthor;
     
-    const pages = document.createElement("p");
-    pages.setAttribute("class", "pages");
-    pages.innerHTML = "<strong>Pages:</strong>" + Bpages;
-
-    const status = document.createElement("p");
-    status.setAttribute("class", "read");
-    //status.innerHTML = "<strong>Status:</strong>" + isRead
-
-    const bookButtons = document.createElement("p");
-    bookButtons.setAttribute("class","book-buttons")
-    bookButtons.innerHTML = '<button class="read-button"><img src="assets/svg/book-open-svgrepo-com.svg" alt="Read" height="25px"></button><button class="remove-button"><img src="assets/svg/trash-xmark-alt-svgrepo-com.svg" alt="Delete" height="25px"></button>';
-
-    booksContainer.appendChild(book);
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(status);
-    book.appendChild(bookButtons);
     booksContainer.appendChild(addBtn);
 }
 
-function addBookToLibrary() {
-  const formData = new FormData(form);
-  console.log(formData);
-  const book = new bookList();
-  book.title = formData.get("title");
-  book.author = formData.get("author");
-  book.pages = formData.get("pages");
-  console.log(book.title,book.author,book.pages)
-  myLibrary.push({title:book.title,
-                  pages:book.pages,
-                  author:book.author,
-                  isRead:true});
-  displayNewBook(book.title,book.author,book.pages)     
-  formContainer.classList.remove("open");
-  container.style.filter = "none";           
 
-}
+submitBtn.addEventListener("click", () =>{
+    addBook();
+    container.style.filter = "none";
+    formContainer.classList.remove("open");
+});
 
-
-
-submitBtn.addEventListener("click" , () =>{
-  addBookToLibrary();
-})
-
-displayBooks();
+readBtn.addEventListener("click", () =>{
+    if (bookRead == false) {
+        readBtn.style.backgroundColor = "green"
+        bookRead = true;
+    }
+    else{
+        readBtn.style.backgroundColor = "red"
+        bookRead = false
+    }
+});
